f4284c9f1b8ad497c5efaa8c39f92444631b9df3
Joonas Kättö <108611102+JoonasKatto@users.noreply.github.com>
2025-05-08 00:08:34 +0300

    Implement input validation in App component and update currency formatting to Euro. Display a warning message for invalid duration input.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import UserInput from './components/UserInput.jsx';
 import Results from './components/Results.jsx';
 
 function App() {
-
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
@@ -25,8 +26,11 @@ function App() {
   return (
     <>
       <Header />
-      <UserInput onChange={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
